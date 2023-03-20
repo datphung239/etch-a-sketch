788ee859111ff0feeabd9e075222171caae858cc
datph <datphung239@gmail.com>
2023-03-20 13:28:17 +0700

    Create a grid contain smaller square to interact

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const container = document.querySelector(".container")
+const containerSize = (getComputedStyle(container).width).replace("px","")
 
+const squares = document.querySelector(".square")
+let squaresNumber = 20
+const squareSize = (containerSize/squaresNumber)
+
+for (sq=1;sq<=squaresNumber*squaresNumber;sq++) {
+    const square = document.createElement("div")
+    square.classList.add("square")
+    container.appendChild(square)
+    square.setAttribute("style",`width:${squareSize}px;height:${squareSize}px`) // Fix this soon for better way
+}
