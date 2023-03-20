63a310bfc4a5990002bbf9a2a0c73e6781c8cfb0
datph <datphung239@gmail.com>
2023-03-20 13:30:28 +0700

    Change container name to grid

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const container = document.querySelector(".container")
-const containerSize = (getComputedStyle(container).width).replace("px","")
+const grid = document.querySelector(".grid")
+const gridSize = (getComputedStyle(grid).width).replace("px","")
 
 const squares = document.querySelector(".square")
 let squaresNumber = 20
-const squareSize = (containerSize/squaresNumber)
+const squareSize = (gridSize/squaresNumber)
 
 for (sq=1;sq<=squaresNumber*squaresNumber;sq++) {
     const square = document.createElement("div")
     square.classList.add("square")
-    container.appendChild(square)
+    grid.appendChild(square)
     square.setAttribute("style",`width:${squareSize}px;height:${squareSize}px`) // Fix this soon for better way
 }
