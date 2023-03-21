8c6568a570d32a4cf26dba038a03d0bc3d446f79
datph <datphung239@gmail.com>
2023-03-21 13:15:24 +0700

    Create pen switchs

--- script.js
+++ script.js
@@ -16,16 +16,51 @@ for (sq=1;sq<=squaresNumber*squaresNumber;sq++) {
     grid.appendChild(square)
 }
 
-// Eraser
-const squares = document.querySelectorAll(".square")
-const eraserBtn = document.querySelector(".eraser")
 
-eraserBtn.addEventListener("click", () => {
-    if (eraserBtn.classList.contains("btn-on")) {
-        return eraserBtn.classList.remove("btn-on")
+
+// Toggle (Use for rainbow, eraser, lighten, shading pen)
+const toggle = document.querySelector("#toggle")
+buttons = toggle.querySelectorAll("button")
+
+toggle.addEventListener("click", (event) => {
+    const toggleBtn = event.target
+    if (toggleBtn.classList.contains("btn")) {
+        // Remove current clicked pen if double click
+        if (toggleBtn.classList.contains("btn-on")) {
+            return toggleBtn.classList.remove("btn-on")
+        }
+        // Delete all pen with button on
+        buttons.forEach((button) => {
+            if (button.classList.contains("btn-on")) {
+                return button.classList.remove("btn-on")
+            }
+        })
+        // Turn button on for current clicked pen
+        toggleBtn.classList.add("btn-on")
+
     }
-    eraserBtn.classList.add("btn-on")
 })
+
+// // Rainbow pen
+// const rainbowBtn = document.querySelector(".rainbow")
+// rainbowBtn.addEventListener("click", () => {
+//     if (rainbowBtn.classList.contains("btn-on")) {
+//         return rainbowBtn.classList.remove("btn-on")
+//     }
+//     rainbowBtn.classList.add("btn-on")
+// })
+
+
+// // Eraser
+// const squares = document.querySelectorAll(".square")
+// const eraserBtn = document.querySelector(".eraser")
+
+// eraserBtn.addEventListener("click", () => {
+//     if (eraserBtn.classList.contains("btn-on")) {
+//         return eraserBtn.classList.remove("btn-on")
+//     }
+//     eraserBtn.classList.add("btn-on")
+// })
 
 // Clear all
 const clearBtn = document.querySelector(".clear")
@@ -40,17 +75,16 @@ clearBtn.addEventListener("click",()=>{
 // Hold to draw
 function holdToDraw(event) {
     if (event.buttons == 1) {
-        event.preventDefault()
+        event.preventDefault() // New to learn
         // Return nothing if not inside of the box
         if (event["target"].className !== "square" ) {
             return
         // Clear the grid
-        } else if (eraserBtn.classList.contains("btn-on")) {
-            if (event["target"].style.cssText) event.target.removeAttribute("style")
+        // } else if (eraserBtn.classList.contains("btn-on")) {
+        //     if (event["target"].style.cssText) event.target.removeAttribute("style")
         // Draw  the grid
         } else if (event["target"].style.backgroundColor!==hexToRgb(squareColor)) {
             event["target"].style.cssText = `background:${squareColor}`
-            console.log("Hello")
         }
     }
 }
